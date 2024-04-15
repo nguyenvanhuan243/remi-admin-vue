02b7d1bb34f2387518cf1d5d485c7f8becf824f5
Huan Nguyen <nguyenvanhuan243@gmail.com>
2024-04-15 10:52:29 +0700

    format login form

--- src/views/pages/Login.vue
+++ src/views/pages/Login.vue
@@ -36,9 +36,7 @@
                       <CButton type="submit" color="primary" class="px-4">Login</CButton>
                     </CCol>
                     <CCol :xs="6" class="text-right">
-                      <CButton color="link" class="px-0" @click="forgotPassword">
-                        Forgot password?
-                      </CButton>
+                      <CButton color="link" class="px-0" @click="forgotPassword">Forgot password?</CButton>
                     </CCol>
                   </CRow>
                   <p v-if="error" class="text-danger mt-2">{{ error }}</p>
@@ -50,9 +48,12 @@
                 <div>
                   <h2>Sign up</h2>
                   <p>Admin System, Manage users, movies</p>
-                  <CButton color="light" variant="outline" class="mt-3" @click="goToRegister">
-                    Register Now!
-                  </CButton>
+                  <CButton
+                    color="light"
+                    variant="outline"
+                    class="mt-3"
+                    @click="goToRegister"
+                  >Register Now!</CButton>
                 </div>
               </CCardBody>
             </CCard>
@@ -64,48 +65,51 @@
 </template>
 
 <script>
-import axios from 'axios';
+import axios from "axios";
 
 export default {
-  name: 'Login',
+  name: "Login",
   data() {
     return {
       formData: {
-        email: '',
-        password: ''
+        email: "",
+        password: ""
       },
-      error: ''
+      error: ""
     };
   },
   methods: {
     async login() {
       try {
-        const response = await axios.post('https://remi-api.onrender.com/api/v1/users/sign_in', {
-          email: this.formData.email,
-          password: this.formData.password
-        });
+        const response = await axios.post(
+          "https://remi-api.onrender.com/api/v1/users/sign_in",
+          {
+            email: this.formData.email,
+            password: this.formData.password
+          }
+        );
 
-        console.log("#############", response.data)
-        
+        console.log("#############", response.data);
+
         // Assuming the API returns a token upon successful login
         const token = response.data;
 
         // Store token in localStorage or Vuex state for authentication
-        localStorage.setItem('accessToken', token);
+        localStorage.setItem("accessToken", token);
 
         // Redirect to dashboard or other authenticated route
-        this.$router.push('/dashboard');
+        this.$router.push("/dashboard");
       } catch (error) {
-        this.error = 'Invalid credentials. Please try again.';
+        this.error = "Invalid credentials. Please try again.";
       }
     },
     forgotPassword() {
       // Implement logic to handle forgot password functionality
-      console.log('Forgot password clicked');
+      console.log("Forgot password clicked");
     },
     goToRegister() {
       // Redirect to registration page
-      this.$router.push('/register');
+      this.$router.push("/register");
     }
   }
 };
