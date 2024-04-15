fcb07c938a20c21719fb6e6984d92af89949b690
Huan Nguyen <nguyenvanhuan243@gmail.com>
2024-04-15 10:47:42 +0700

    integrate login

--- src/views/pages/Login.vue
+++ src/views/pages/Login.vue
@@ -6,7 +6,7 @@
           <CCardGroup>
             <CCard class="p-4">
               <CCardBody>
-                <CForm>
+                <CForm @submit.prevent="login">
                   <h1>Login</h1>
                   <p class="text-body-secondary">Sign In to your account</p>
                   <CInputGroup class="mb-3">
@@ -14,7 +14,9 @@
                       <CIcon icon="cil-user" />
                     </CInputGroupText>
                     <CFormInput
-                      placeholder="Username"
+                      v-model="formData.email"
+                      type="email"
+                      placeholder="Email"
                       autocomplete="username"
                     />
                   </CInputGroup>
@@ -23,6 +25,7 @@
                       <CIcon icon="cil-lock-locked" />
                     </CInputGroupText>
                     <CFormInput
+                      v-model="formData.password"
                       type="password"
                       placeholder="Password"
                       autocomplete="current-password"
@@ -30,14 +33,15 @@
                   </CInputGroup>
                   <CRow>
                     <CCol :xs="6">
-                      <CButton color="primary" class="px-4"> Login </CButton>
+                      <CButton type="submit" color="primary" class="px-4">Login</CButton>
                     </CCol>
                     <CCol :xs="6" class="text-right">
-                      <CButton color="link" class="px-0">
+                      <CButton color="link" class="px-0" @click="forgotPassword">
                         Forgot password?
                       </CButton>
                     </CCol>
                   </CRow>
+                  <p v-if="error" class="text-danger mt-2">{{ error }}</p>
                 </CForm>
               </CCardBody>
             </CCard>
@@ -45,10 +49,8 @@
               <CCardBody class="text-center">
                 <div>
                   <h2>Sign up</h2>
-                  <p>
-                    Admin System, Manage users, movies
-                  </p>
-                  <CButton color="light" variant="outline" class="mt-3">
+                  <p>Admin System, Manage users, movies</p>
+                  <CButton color="light" variant="outline" class="mt-3" @click="goToRegister">
                     Register Now!
                   </CButton>
                 </div>
@@ -62,7 +64,49 @@
 </template>
 
 <script>
+import axios from 'axios';
+
 export default {
   name: 'Login',
-}
+  data() {
+    return {
+      formData: {
+        email: '',
+        password: ''
+      },
+      error: ''
+    };
+  },
+  methods: {
+    async login() {
+      try {
+        const response = await axios.post('https://remi-api.onrender.com/api/v1/users/sign_in', {
+          email: this.formData.email,
+          password: this.formData.password
+        });
+
+        console.log("#############", response.data)
+        
+        // Assuming the API returns a token upon successful login
+        const token = response.data;
+
+        // Store token in localStorage or Vuex state for authentication
+        localStorage.setItem('accessToken', token);
+
+        // Redirect to dashboard or other authenticated route
+        this.$router.push('/dashboard');
+      } catch (error) {
+        this.error = 'Invalid credentials. Please try again.';
+      }
+    },
+    forgotPassword() {
+      // Implement logic to handle forgot password functionality
+      console.log('Forgot password clicked');
+    },
+    goToRegister() {
+      // Redirect to registration page
+      this.$router.push('/register');
+    }
+  }
+};
 </script>
