65d7fd132354fe06e52ec9e7a8ff5ad33de31884
Huan Nguyen <nguyenvanhuan243@gmail.com>
2024-04-15 11:53:59 +0700

    update dashboard view

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -1,14 +1,16 @@
 <template>
   <CFooter class="px-4">
     <div>
-      <a href="https://coreui.io" target="_blank">CoreUI</a>
+      <a href="https://github.com/nguyenvanhuan243" target="_blank">
+        Admin Vuejs
+      </a>
       <span class="ms-1"
-        >&copy; {{ new Date().getFullYear() }} creativeLabs.</span
+        >&copy; {{ new Date().getFullYear() }} AdminVueJS.</span
       >
     </div>
     <div class="ms-auto">
-      <span class="me-1" target="_blank">Powered by</span>
-      <a href="https://coreui.io/vue">CoreUI for Vue</a>
+      <span class="me-1">Powered by</span>
+      <a target="_blank" href="https://github.com/nguyenvanhuan243">Admin for Vue</a>
     </div>
   </CFooter>
 </template>
